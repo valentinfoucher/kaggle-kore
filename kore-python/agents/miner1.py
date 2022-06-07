4df44d6140120d6084c7f5a74e7c99e3df1c3ed9
iMac <imac@imac-de-barbara.home>
2022-06-08 00:49:55 +0200

    added few agents

--- kore-python/agents/miner1.py
+++ kore-python/agents/miner1.py
@@ -21,8 +21,10 @@ def agent(obs, config):
     convert_cost = board.configuration.convert_cost
     remainingKore = me.kore
 
+    attack_flight_plan = "W10N"
+
     for shipyard in me.shipyards:
-        if(remainingKore > 1000 and shipyard.max_spawn > 5):
+        if(remainingKore > 200 and shipyard.max_spawn > 5):
             if(shipyard.ship_count >= convert_cost + 10):
                 gap1 = getRandomInt(3, 9)
                 gap2 = getRandomInt(3, 9)
@@ -48,8 +50,8 @@ def agent(obs, config):
                 shipyard.next_action = action
 
         elif(shipyard.ship_count >= 21):
-            gap1 = getRandomInt(3, 9)
-            gap2 = getRandomInt(3, 9)
+            gap1 = getRandomInt(2, 4)
+            gap2 = getRandomInt(2, 4)
             startDir = getRandomInt(0, 3)
             flightPlan = Direction.list_directions(
             )[startDir].to_char() + str(gap1)
@@ -71,8 +73,10 @@ def agent(obs, config):
                     min(shipyard.max_spawn, math.floor(remainingKore / spawn_cost)))
                 shipyard.next_action = action
 
-        elif(shipyard.ship_count >= 2):
-            dirStr = Direction.random_direction().to_char()
-            action = ShipyardAction.launch_fleet_with_flight_plan(2, dirStr)
-            shipyard.next_action = action
+        # elif(shipyard.ship_count >= 2):
+        #     dirStr = Direction.random_direction().to_char()
+        #     count_spawn = shipyard.ship_count
+        #     action = ShipyardAction.launch_fleet_with_flight_plan(
+        #         count_spawn, attack_flight_plan)
+        #     shipyard.next_action = action
     return me.next_actions
--- kore-python/agents/miner3.py
+++ kore-python/agents/miner3.py
@@ -6,9 +6,36 @@ from kaggle_environments.envs.kore_fleets.helpers import *
 from random import randint
 import math
 
+possible_sizes_fleet = [2, 3, 5, 8, 13, 21, 34]
+
 
 def getRandomInt(min, max):
     return random.randint(min, max)
+
+
+def launch_rectangle_ship(shipyard, board):
+    turn = board.step
+    max_length = min(1+turn//20, 9)
+    gap1 = getRandomInt(0, max_length)
+    gap2 = getRandomInt(0, max_length)
+    startDir = getRandomInt(0, 3)
+    flightPlan = Direction.list_directions(
+    )[startDir].to_char() + str(gap1)
+    nextDir = (startDir + 1) % 4
+    flightPlan += Direction.list_directions(
+    )[nextDir].to_char() + str(gap2)
+    nextDir2 = (nextDir + 1) % 4
+    flightPlan += Direction.list_directions()[
+        nextDir2].to_char() + str(gap1)
+    nextDir3 = (nextDir2 + 1) % 4
+    flightPlan += Direction.list_directions()[nextDir3].to_char()
+
+    fleet_size = max(
+        [s for s in possible_sizes_fleet if s <= shipyard.ship_count])
+
+    action = ShipyardAction.launch_fleet_with_flight_plan(
+        fleet_size, flightPlan)
+    shipyard.next_action = action
 
 
 def agent(obs, config):
@@ -21,8 +48,10 @@ def agent(obs, config):
     convert_cost = board.configuration.convert_cost
     remainingKore = me.kore
 
+    # possible_sizes_fleet = [2, 3, 5, 8, 13, 21, 34]
+
     for shipyard in me.shipyards:
-        if(remainingKore > 1000 and shipyard.max_spawn > 5):
+        if(remainingKore > 200 and shipyard.max_spawn > 5):
             if(shipyard.ship_count >= convert_cost + 10):
                 gap1 = getRandomInt(3, 9)
                 gap2 = getRandomInt(3, 9)
@@ -48,22 +77,28 @@ def agent(obs, config):
                 shipyard.next_action = action
 
         elif(shipyard.ship_count >= 21):
-            gap1 = getRandomInt(3, 9)
-            gap2 = getRandomInt(3, 9)
-            startDir = getRandomInt(0, 3)
-            flightPlan = Direction.list_directions(
-            )[startDir].to_char() + str(gap1)
-            nextDir = (startDir + 1) % 4
-            flightPlan += Direction.list_directions(
-            )[nextDir].to_char() + str(gap2)
-            nextDir2 = (nextDir + 1) % 4
-            flightPlan += Direction.list_directions()[
-                nextDir2].to_char() + str(gap1)
-            nextDir3 = (nextDir2 + 1) % 4
-            flightPlan += Direction.list_directions()[nextDir3].to_char()
-            action = ShipyardAction.launch_fleet_with_flight_plan(
-                21, flightPlan)
-            shipyard.next_action = action
+            launch_rectangle_ship(shipyard, board)
+            # max_length = min(1+turn//20, 9)
+            # gap1 = getRandomInt(0, max_length)
+            # gap2 = getRandomInt(0, max_length)
+            # startDir = getRandomInt(0, 3)
+            # flightPlan = Direction.list_directions(
+            # )[startDir].to_char() + str(gap1)
+            # nextDir = (startDir + 1) % 4
+            # flightPlan += Direction.list_directions(
+            # )[nextDir].to_char() + str(gap2)
+            # nextDir2 = (nextDir + 1) % 4
+            # flightPlan += Direction.list_directions()[
+            #     nextDir2].to_char() + str(gap1)
+            # nextDir3 = (nextDir2 + 1) % 4
+            # flightPlan += Direction.list_directions()[nextDir3].to_char()
+
+            # fleet_size = max(
+            #     [s for s in possible_sizes_fleet if s <= shipyard.ship_count])
+
+            # action = ShipyardAction.launch_fleet_with_flight_plan(
+            #     fleet_size, flightPlan)
+            # shipyard.next_action = action
         elif(remainingKore > board.configuration.spawn_cost * shipyard.max_spawn):
             remainingKore -= board.configuration.spawn_cost
             if(remainingKore >= spawn_cost):
@@ -73,6 +108,8 @@ def agent(obs, config):
 
         elif(shipyard.ship_count >= 2):
             dirStr = Direction.random_direction().to_char()
-            action = ShipyardAction.launch_fleet_with_flight_plan(2, dirStr)
+            count_spawn = shipyard.ship_count
+            action = ShipyardAction.launch_fleet_with_flight_plan(
+                count_spawn, dirStr)
             shipyard.next_action = action
     return me.next_actions
